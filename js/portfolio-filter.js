127a76feae4c8c66c1c2a0eb0d2245775a7a2aa1
MilBra <branislav.milosevic.91@gmail.com>
2018-11-27 21:20:24 +0100

    version 2
    New change

---
+++ js/portfolio-filter.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('2000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('4000');
+            $('.filter').filter('.'+value).show('4000');
+            
+        }
+    });
+    
+});
