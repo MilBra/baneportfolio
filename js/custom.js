82ae7dd7043ecf1598ca663d544725c6bedd28f5
MilBra <branislav.milosevic.91@gmail.com>
2018-12-14 11:35:54 +0100

    Update (mobile)

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,19 @@ function poruka() {
 	alert ("I work on my contact form validation - please send me massage on: branislav.milosevic.91@gmail.com");
 }
 
+
+// code for menu navigation
+
+$(document).ready(function(){
+	$("#mobile_Left_Menu").click(function(){
+	  $(".leviMeni").fadeToggle("fast");
+	});
+});
+
+$(document).ready(function(){
+	$("#mobile_Right_Menu").click(function(){
+	  $(".desniMeni").fadeToggle("fast");
+	});
+});
+
+
